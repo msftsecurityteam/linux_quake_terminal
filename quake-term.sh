607aafc6e55d01f0abaf5b8d581b917ffa2a9981
mike <darkhat@gmail.com>
2025-01-29 05:36:13 -0800

    fixed a bug where sometimes in a multiple external monitor scenario, the terminal would go across all or multiple monitors

--- quake-term.sh
+++ quake-term.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# quake-terminal.sh
+# quake-term.sh
 #
 # Copyright (c) 2025 Michael Cramp
 #
@@ -9,17 +9,20 @@
 # in the root of this repository for more information.
 #
 
-TERMINAL_CLASS="kitty_launcher"
+TERMINAL_CLASS="kitty_launcher" # special classname for your quake terminal
+ANIMATION_DURATION=0.0625       # time (seconds) you would like it to take to open the terminal
+ANIMATION_STEPS=30              # can leave this largely untouched
+MIN_HEIGHT_PERCENT=1            # keep this at 1 (unless you know what you're doing)
+MAX_HEIGHT_PERCENT=30           # adjust this higher or lower to change the size of your quake-terminal
+
 WINDOW_ID=$(xdotool search --classname $TERMINAL_CLASS)
-
 if [ -z "$WINDOW_ID" ]; then
-  kitty --name kitty_launcher --start-as minimized
+  # Start the terminal if it isn't already running.
+  kitty --name "$TERMINAL_CLASS" --start-as minimized --detach
+  # Leave a short delay to let the terminal initialize.
+  sleep 0.1
+  WINDOW_ID=$(xdotool search --classname $TERMINAL_CLASS)
 fi
-
-ANIMATION_DURATION=0.0625
-MAX_HEIGHT_PERCENT=30
-ANIMATION_STEPS=30
-MIN_HEIGHT_PERCENT=1
 
 # Check if a window is minimized (Withdrawn)
 is_withdrawn() {
@@ -42,8 +45,24 @@ get_current_height_percent() {
   echo "$HEIGHT_PERCENT"
 }
 
+# Get current monitor width
+get_monitor_width() {
+  eval $(xdotool getmouselocation --shell)
+  ACTIVE_MONITOR=$(xrandr --query | grep " connected" | awk -v x=$X -v y=$Y '{
+    split($3, coords, "+");
+    split(coords[1], dimensions, "x");
+    if (x >= coords[2] && x <= coords[2] + dimensions[1] && y >= coords[3] && y <= coords[3] + dimensions[2]) {
+      print $1";"dimensions[1]
+      exit
+    }
+  }')
+  echo ${ACTIVE_MONITOR##*;}
+}
+
 # Toggle the dropdown terminal
 toggle_terminal() {
+  LOCAL_WIDTH=$(get_monitor_width)
+
   if is_withdrawn "$WINDOW_ID"; then
     # If minimized, start at 1% height
     xdotool windowmap "$WINDOW_ID"
@@ -55,7 +74,7 @@ toggle_terminal() {
   if [ "$CURRENT_HEIGHT_PERCENT" -eq "$MAX_HEIGHT_PERCENT" ]; then
     # If at maximum height, contract to minimum and minimize
     for ((i = MAX_HEIGHT_PERCENT; i >= MIN_HEIGHT_PERCENT; i--)); do
-      xdotool windowsize "$WINDOW_ID" 100% "$i%"
+      xdotool windowsize "$WINDOW_ID" "$LOCAL_WIDTH" "$i%"
       sleep $(awk "BEGIN {print $ANIMATION_DURATION / $ANIMATION_STEPS}")
     done
     # Minimize the window
@@ -63,7 +82,7 @@ toggle_terminal() {
   else
     # Expand from current height to maximum height
     for ((i = CURRENT_HEIGHT_PERCENT; i <= MAX_HEIGHT_PERCENT; i++)); do
-      xdotool windowsize "$WINDOW_ID" 100% "$i%"
+      xdotool windowsize "$WINDOW_ID" "$LOCAL_WIDTH" "$i%"
       sleep $(awk "BEGIN {print $ANIMATION_DURATION / $ANIMATION_STEPS}")
     done
   fi
